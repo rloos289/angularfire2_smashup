ce591a9241233707621f62f8af74c1a7a08da3be
Ryan Loos <RLoos289@gmail.com>
2016-11-05 21:58:35 -0700

    got basic angularfire functionality to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <div class="deck" *ngFor="let deck of models | async">
+      <p>Deck Name: {{deck.name}}</p>
+      <p>Set: {{deck.set}}</p>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  models: FirebaseListObservable<any[]>;
+  constructor(angularFire: AngularFire){
+    this.models = angularFire.database.list('decks');
+
+
+  }
+  title = 'Smashup Utility';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAE1bhZLEOMZRsgvNnWYXI73tiWjhqO1ug",
+    authDomain: "smashup-e7242.firebaseapp.com",
+    databaseURL: "https://smashup-e7242.firebaseio.com",
+    storageBucket: "smashup-e7242.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
